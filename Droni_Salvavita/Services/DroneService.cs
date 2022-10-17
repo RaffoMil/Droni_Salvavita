c184148ab9dad868913441ff5132d724edcc0691
Raffaele_Milone <raffaele.milone@live.it>
2022-10-17 15:19:22 +0200

    Refactoring Metodo GetTimeSpan

--- Droni_Salvavita/Services/DroneService.cs
+++ Droni_Salvavita/Services/DroneService.cs
@@ -4,16 +4,9 @@ namespace Droni_Salvavita.Services
 {
     public class DroneService
     {
-        private  Drone drone = new Drone();
-        private  Volo volo = new Volo();
-
-        
-        
-
-        private TimeSpan GetTimeSpan (DateTime DepartureDate, DateTime ArrivalDate)
-        {
-            drone.FlightTime = ArrivalDate - DepartureDate;
-            return drone.FlightTime;
+        private TimeSpan GetTimeSpan(DateTime DepartureDate, DateTime ArrivalDate) {
+            TimeSpan FlightTime = ArrivalDate - DepartureDate;
+            return FlightTime;
         }
 
 
