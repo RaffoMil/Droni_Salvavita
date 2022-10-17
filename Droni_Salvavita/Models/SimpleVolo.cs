869d727a21311200ecc6d70c4ad9d78dc641ad5e
Raffaele_Milone <raffaele.milone@live.it>
2022-10-17 16:04:37 +0200

    Inserimento Classe SimpleVolo Refactoring

---
+++ Droni_Salvavita/Models/SimpleVolo.cs
@@ -0,0 +1,11 @@
+﻿namespace Droni_Salvavita.Models
+{
+    public class SimpleVolo
+    {
+        public int FlightId { get; set; }
+
+        public DateTime DepartureDate { get; set; }
+
+        public DateTime ArrivalDate { get; set; }
+    }
+}
