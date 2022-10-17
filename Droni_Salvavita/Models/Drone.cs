e666524f15c7b0bc4bed4cc197b158a60aedf98f
Raffaele_Milone <raffaele.milone@live.it>
2022-10-17 10:30:35 +0200

    Creazione Modello Drone

---
+++ Droni_Salvavita/Models/Drone.cs
@@ -0,0 +1,24 @@
+﻿namespace Droni_Salvavita.Models
+{
+    public class Drone
+    {
+        public enum TipoPropulsore {
+            PLASMA = 1,
+            NUCLEARE = 2,
+            ELETTIRICO = 3,
+        }
+
+        public enum TipoGuida {
+            SPERICOLATA = 1,
+            CODICEVERDE = 2,
+            CODICEGIALLO = 3,
+            CODICEROSSO = 4,
+        }
+
+        public int Id { get; set; }
+        public TimeSpan Duration { get; set; }
+        public TipoPropulsore TypePropulsore { get; set; }
+        public TipoGuida TypeGuida { get; set; }
+
+    }
+}
